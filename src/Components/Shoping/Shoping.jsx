63583ec62e683bfc000e95a36470efe5efd3f521
DevIsmail-coder <ismailolawale777@gmail.com>
2025-05-06 09:56:06 +0100

    done

--- src/Components/Shoping/Shoping.jsx
+++ src/Components/Shoping/Shoping.jsx
@@ -17,7 +17,6 @@ const Shoping = () => {
         }
     });
 
-    // Save cart to localStorage whenever it changes
     useEffect(() => {
         localStorage.setItem('cart', JSON.stringify(cart));
     }, [cart]);
